e3347ed3af47a656ebef6d0db06ef2b714da7dec
Rikky J <rikkyj108@gmail.com>
2026-08-27 11:50:45 +0530

    static site generation

--- app/(application)/blogs/[blogID]/page.js
+++ app/(application)/blogs/[blogID]/page.js
@@ -1,6 +1,11 @@
 import { notFound } from 'next/navigation';
 
-
+export async function generateStaticParams() {
+    return [
+        { blogID: '1' },
+        { blogID: '2' },
+    ];
+}
 
 export async function generateMetadata({ params }) {
     const { blogID } = await params;
