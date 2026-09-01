89c51641f386382ccb21f21fa22299e6d04a20f9
Rikky J <rikkyj108@gmail.com>
2026-09-01 12:18:09 +0530

    -

--- app/posts/page.js
+++ app/posts/page.js
@@ -1,4 +1,14 @@
 const Posts = () => {
+    const [posts, setPosts] = useState([]);
+    useEffect(() => {
+        async function feachPosts(){
+            const responnce = await fearch(
+                "https://jsonplaceholder.typicode.com/posts"
+            );
+            const data = await responce.json();
+            console.log(data);
+        }
+    })
     return (
         <>
             <h1>Posts</h1>
