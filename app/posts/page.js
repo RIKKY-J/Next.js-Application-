f218b9c77854ac87ffb9e3528a2bcacb7eed5251
Rikky J <rikkyj108@gmail.com>
2026-09-01 12:19:52 +0530

    -

--- app/posts/page.js
+++ app/posts/page.js
@@ -6,13 +6,19 @@ const Posts = () => {
                 "https://jsonplaceholder.typicode.com/posts"
             );
             const data = await responce.json();
-            console.log(data);
+            setPosts(data);
         }
         featchPosts();
-    })
+    }, []);
     return (
         <>
             <h1>Posts</h1>
+            {posts.map(post => (
+                <div key={post.id}>
+                    <h2>{post.title}</h2>
+                    <p>{post.body}</p>
+                </div>
+            ))}
         </>
     )
 }
